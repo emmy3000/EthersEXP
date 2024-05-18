df0368f5f428f46d11e0889edc2932766e280b01
Emeka Emodi <emodiemeka@gmail.com>
2024-05-18 11:24:00 -0400

    fix: correct syntax for 'require' function in '1_accounts.js'

--- examples/1_accounts.js
+++ examples/1_accounts.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 require('dotenv').config();
-const { ethers } = requires("ethers");
+const { ethers } = require("ethers");
 
 const provider = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/ETHCONNECT_API_KEY");
+console.log(provider);
