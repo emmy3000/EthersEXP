2767fa7750c38ca6f75e4ec7de2e996266654af2
Emeka Emodi <emodiemeka@gmail.com>
2024-05-18 15:31:09 -0400

    refactor: Use process.env for managing environment variables

--- examples/1_accounts.js
+++ examples/1_accounts.js
@@ -3,5 +3,8 @@
 require('dotenv').config();
 const { ethers } = require("ethers");
 
-const provider = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/ETHCONNECT_API_KEY");
-console.log(provider);
+const provider = new ethers.providers.JsonRpcProvider(process.env.INFURA_API_ENDPOINT);
+
+
+const balance = provider.getBalance('0x0D2a409602031Ff5843e3ed4C39a4ce98295E071');
+console.log(balance);
