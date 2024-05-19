a6439e7cc44daa0f3a9e79e13d032f0029ac4273
Emeka Emodi <emodiemeka@gmail.com>
2024-05-19 01:23:17 -0400

    feat: Add async balance retrieval function.

--- examples/1_accounts.js
+++ examples/1_accounts.js
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
 
 require('dotenv').config();
-const { ethers } = require("ethers");
+const { ethers } = require('ethers');
 
-const provider = new ethers.providers.JsonRpcProvider(process.env.INFURA_API_ENDPOINT);
+const provider = new ethers.providers.JsonRpcProvider(
+  process.env.ETHCONNECT_API_ENDPOINT
+);
 
+const walletAddress = '0xb5aD18d7BF5e0D4C293Ab71745fA14C99A094C36';
 
-const balance = provider.getBalance('0x0D2a409602031Ff5843e3ed4C39a4ce98295E071');
-console.log(balance);
+const main = async () => {
+  const balance = await provider.getBalance(walletAddress);
+  console.log(`\nETH Balance of ${walletAddress} ==> ${balance.toString()}\n`);
+};
+
+main();
