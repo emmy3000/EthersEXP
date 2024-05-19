b07b7dff5fa6d5af52d7ad4d6c309a62ee456d3d
Emeka Emodi <emodiemeka@gmail.com>
2024-05-19 14:23:55 -0400

    feat: Improve Ethereum balance display with formatted ETH output.

--- examples/1_accounts.js
+++ examples/1_accounts.js
@@ -11,7 +11,15 @@ const walletAddress = '0xb5aD18d7BF5e0D4C293Ab71745fA14C99A094C36';
 
 const main = async () => {
   const balance = await provider.getBalance(walletAddress);
-  console.log(`\nETH Balance of ${walletAddress} ==> ${balance.toString()}\n`);
+  const balanceInEther = ethers.utils.formatEther(balance);
+  console.log(
+    `\n\n\x1b[32m=============\x1b[0m ` +
+      `\x1b[1m\x1b[36m\x1b[3mETHEREUM MAINNET BALANCE REPORT\x1b[0m` +
+      ` \x1b[32m============\x1b[0m \n\n` +
+      `\x1b[1m\x1b[33mWallet Address:\x1b[0m ${walletAddress}\n` +
+      `\x1b[1m\x1b[33mCurrent Balance:\x1b[0m ${balanceInEther} \x1b[1m\x1b[33mETH\x1b[0m\n\n` +
+      `\x1b[32m==========================================================\x1b[0m\n\n`
+  );
 };
 
 main();
