ee80aa30bf3c0f5fc8f7029affa7b5ecdf976490
Emeka Emodi <emodiemeka@gmail.com>
2024-05-20 08:21:03 -0400

    feat: Implement 'try-catch' block for Ethereum balance retrieval error handling.

--- examples/1_accounts.js
+++ examples/1_accounts.js
@@ -10,16 +10,26 @@ const provider = new ethers.providers.JsonRpcProvider(
 const walletAddress = '0xb5aD18d7BF5e0D4C293Ab71745fA14C99A094C36';
 
 const main = async () => {
-  const balance = await provider.getBalance(walletAddress);
-  const balanceInEther = ethers.utils.formatEther(balance);
-  console.log(
-    `\n\n\x1b[32m=============\x1b[0m ` +
-      `\x1b[1m\x1b[36m\x1b[3mETHEREUM MAINNET BALANCE REPORT\x1b[0m` +
-      ` \x1b[32m============\x1b[0m \n\n` +
-      `\x1b[1m\x1b[33mWallet Address:\x1b[0m ${walletAddress}\n` +
-      `\x1b[1m\x1b[33mCurrent Balance:\x1b[0m ${balanceInEther} \x1b[1m\x1b[33mETH\x1b[0m\n\n` +
-      `\x1b[32m==========================================================\x1b[0m\n\n`
-  );
+  try {
+    const balance = await provider.getBalance(walletAddress);
+    const balanceInEther = ethers.utils.formatEther(balance);
+    console.log(
+      `\n\n\x1b[32m=============\x1b[0m ` +
+        `\x1b[1m\x1b[36m\x1b[3mETHEREUM MAINNET BALANCE REPORT\x1b[0m` +
+        ` \x1b[32m============\x1b[0m \n\n` +
+        `\x1b[1m\x1b[33mWallet Address:\x1b[0m ${walletAddress}\n` +
+        `\x1b[1m\x1b[33mCurrent Balance:\x1b[0m ${balanceInEther} \x1b[1m\x1b[33mETH\x1b[0m\n\n` +
+        `\x1b[32m==========================================================\x1b[0m\n\n`
+    );
+  } catch (error) {
+    console.error(
+      '\x1b[31m\x1b[1mError: Failed to retrieve Ethereum balance\x1b[0m\n\n',
+      'Reason:',
+      error.message,
+      '\n',
+      'Please check your API endpoint, wallet address, and network connection.'
+    );
+  }
 };
 
 main();
