405b4e6daacecb7f0eeb58ec865a71601aa46d9b
Emeka Emodi <emodiemeka@gmail.com>
2024-05-21 07:05:09 -0400

    feat: Add informative comments.

--- examples/1_accounts.js
+++ examples/1_accounts.js
@@ -1,18 +1,25 @@
 #!/usr/bin/env node
 
+// 🌟 Load env vars
 require('dotenv').config();
 const { ethers } = require('ethers');
 
+// 🔌 Create Ethereum provider
 const provider = new ethers.providers.JsonRpcProvider(
   process.env.ETHCONNECT_API_ENDPOINT
 );
 
-const walletAddress = '0xb5aD18d7BF5e0D4C293Ab71745fA14C99A094C36';
+// 🏦 Client wallet address
+const walletAddress = '0x4754B849DC5164C4e09f5dE6639077438A94d205';
 
+// ⚙️  Main event loop unit
 const main = async () => {
   try {
+    // 💰 Get wallet balance, convert WEI --> ETH
     const balance = await provider.getBalance(walletAddress);
     const balanceInEther = ethers.utils.formatEther(balance);
+
+    // 📊 Log wallet address and balance
     console.log(
       `\n\n\x1b[32m=============\x1b[0m ` +
         `\x1b[1m\x1b[36m\x1b[3mETHEREUM MAINNET BALANCE REPORT\x1b[0m` +
@@ -22,6 +29,7 @@ const main = async () => {
         `\x1b[32m==========================================================\x1b[0m\n\n`
     );
   } catch (error) {
+    // 🛑 Handle errors
     console.error(
       '\x1b[31m\x1b[1mError: Failed to retrieve Ethereum balance\x1b[0m\n\n',
       'Reason:',
@@ -32,4 +40,5 @@ const main = async () => {
   }
 };
 
+// 🚀 Execute!
 main();
