c8d7e940f4a6e95f282ac563d4189b21898e4309
Emeka Emodi <emodiemeka@gmail.com>
2024-05-24 13:48:03 -0400

    feat: Add basic read functions for DAI contract.

--- examples/2_read_smart_contract.js
+++ examples/2_read_smart_contract.js
@@ -14,22 +14,30 @@ const provider = new ethers.providers.JsonRpcProvider(
 
 // DAI Smart Contract's ABI
 const ERC20_ABI = [
-    "function name() view returns (string)",
-    "function symbol() view returns (string)",
-    "function totalSupply() view returns (uint256)",
-    "function balanceOf(address) view returns (uint)",
+  'function name() view returns (string)',
+  'function symbol() view returns (string)',
+  'function totalSupply() view returns (uint256)',
+  'function balanceOf(address) view returns (uint)',
 ];
 
 // Define the DAI Contract's address
-const address = '0x4754B849DC5164C4e09f5dE6639077438A94d205';
+const address = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
 
 // Instantiate a new Ethers contract
 const contract = new ethers.Contract(address, ERC20_ABI, provider);
 
-// Main app logic
+// Main app logic holding basic read function definitions
 const main = async () => {
+  const name = await contract.name();
+  const symbol = await contract.symbol();
+  const totalSupply = await contract.totalSupply();
 
+  console.log(
+    `\n\x1b[1m\x1b[36mReading from \x1b[33m${address}\x1b[0m\x1b[36m:\x1b[0m`
+  );
+  console.log(`\x1b[32m- Name: \x1b[37m${name}\x1b[0m`);
+  console.log(`\x1b[32m- Symbol: \x1b[37m${symbol}\x1b[0m`);
+  console.log(`\x1b[32m- Total Supply: \x1b[37m${totalSupply}\x1b[0m\n`);
 };
 
-// Run main function
 main();
