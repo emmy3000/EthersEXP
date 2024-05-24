2128928c8b87d2e3a58ac4cb9535957e4824d91b
Emeka Emodi <emodiemeka@gmail.com>
2024-05-24 13:15:20 -0400

    feat: Add ABI definition for DAI smart contract.

--- examples/2_read_smart_contract.js
+++ examples/2_read_smart_contract.js
@@ -12,6 +12,14 @@ const provider = new ethers.providers.JsonRpcProvider(
   process.env.ETHCONNECT_API_ENDPOINT
 );
 
+// DAI Smart Contract's ABI
+const ERC20_ABI = [
+    "function name() view returns (string)",
+    "function symbol() view returns (string)",
+    "function totalSupply() view returns (uint256)",
+    "function balanceOf(address) view returns (uint)",
+];
+
 // Define the DAI Contract's address
 const address = '0x4754B849DC5164C4e09f5dE6639077438A94d205';
 
