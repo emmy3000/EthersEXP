aa3fbb3c80d9aa48735dfd6c9fd613c45344b706
Emeka Emodi <emodiemeka@gmail.com>
2024-05-25 08:18:56 -0400

    Refactor transaction details output and concise comments.

--- examples/3_send_signed_transaction.js
+++ examples/3_send_signed_transaction.js
@@ -1,69 +1,73 @@
 #!/usr/bin/env node
 
-// Limit event listeners to prevent memory leaks
+// Prevent memory leaks
 require('events').EventEmitter.setMaxListeners(20);
 
-// Load env vars and Ethers.js
+// Load environment variables and Ethers.js
 require('dotenv').config();
 const { ethers } = require('ethers');
 
-// Initialize the Testnet JSON-RPC provider
+// Initialize Testnet JSON-RPC provider
 const provider = new ethers.providers.JsonRpcProvider(
   process.env.ETHCONNECT_TESTNET_API_ENDPOINT
 );
 
-// Define the sender account
+// Define sender and recipient accounts
 const accountA = '0x8B78368464b516b93F6cB331B4515dB509aF6D06';
-
-// Define the recipient account
 const accountB = '0x0D2a409602031Ff5843e3ed4C39a4ce98295E071';
 
-// Define the sender's private key
+// Define sender's private key
 const privateKey = process.env.WALLET_PRIVATE_KEY;
 
-// Create a Wallet instance with the private key and a provider
+// Create Wallet instance with private key and provider
 const wallet = new ethers.Wallet(privateKey, provider);
 
 // Main app logic
 const main = async () => {
-  // Show accountA and accountB balance before transfer
-  const senderBalanceBefore = await provider.getBalance(accountA);
-  const receiverBalanceBefore = await provider.getBalance(accountB);
+  // Get balances before transfer
+  const [senderBalanceBefore, receiverBalanceBefore] = await Promise.all([
+    provider.getBalance(accountA),
+    provider.getBalance(accountB),
+  ]);
 
+  // Print initial balances
   console.log(
-    `\n\n\x1b[1m\x1b[36mSender balance before: \x1b[37m${ethers.utils.formatEther(
-      senderBalanceBefore
-    )}`
+    '\n\n\x1b[1m\x1b[36mSender: \x1b[37m',
+    ethers.utils.formatEther(senderBalanceBefore),
+    '\x1b[33mETH'
   );
   console.log(
-    `\x1b[1m\x1b[36mRecipient balance before: \x1b[37m${ethers.utils.formatEther(
-      receiverBalanceBefore
-    )}\n`
+    '\x1b[1m\x1b[36mRecipient: \x1b[37m',
+    ethers.utils.formatEther(receiverBalanceBefore),
+    '\x1b[33mETH\n'
   );
 
   // Send 0.025 Ether to accountB
   const tx = await wallet.sendTransaction({
     to: accountB,
-    value: ethers.utils.parseEther('0.001'),
+    value: ethers.utils.parseEther('0.025'),
   });
 
-  // Wait for the transaction to be mined
+  // Wait for transaction to be mined
   await tx.wait();
   console.log(tx);
 
-  // Show accountA and accountB balance after transfer
-  const senderBalanceAfter = await provider.getBalance(accountA);
-  const receiverBalanceAfter = await provider.getBalance(accountB);
+  // Get balances after transfer
+  const [senderBalanceAfter, receiverBalanceAfter] = await Promise.all([
+    provider.getBalance(accountA),
+    provider.getBalance(accountB),
+  ]);
 
+  // Print updated balances
   console.log(
-    `\n\x1b[1m\x1b[36mSender balance after: \x1b[37m${ethers.utils.formatEther(
-      senderBalanceAfter
-    )}`
+    '\n\x1b[1m\x1b[36mSender: \x1b[37m',
+    ethers.utils.formatEther(senderBalanceAfter),
+    '\x1b[33mETH'
   );
   console.log(
-    `\x1b[1m\x1b[36mRecipient balance after: \x1b[37m${ethers.utils.formatEther(
-      receiverBalanceAfter
-    )}\n\n`
+    '\x1b[1m\x1b[36mRecipient: \x1b[37m',
+    ethers.utils.formatEther(receiverBalanceAfter),
+    '\x1b[33mETH\n\n'
   );
 };
 
